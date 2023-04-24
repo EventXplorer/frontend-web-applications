1512ece52354dd7fb4d77b5b922e8d7fad6bf052
FaridIzquierdo <u202015215@upc.edu.pe>
2023-04-24 10:17:34 -0500

    First commit

---
+++ shared/material.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+
+//Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatSidenavModule,
+        MatTableModule
+    ],
+    exports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatSidenavModule,
+        MatTableModule
+    ]
+    
+  })
+  export class MaterialModule { }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from 'shared/material.module'
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
