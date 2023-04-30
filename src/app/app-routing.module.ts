dd4a965bba5e6a53fa841bb7a56c058841902890
JennyBalceda <89040419+JennyBalceda@users.noreply.github.com>
2023-04-30 12:07:52 -0500

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterInformationComponent } from './components/register-information/register-information.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { NewPasswordComponent } from './components/new-password/new-password.component';
-
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'login'},
-  {path: '**', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'login', component:LoginComponent},
-  {path: 'register', component:RegisterComponent},
-  {path: 'register/information', component:RegisterInformationComponent},
-  {path: 'forgotpassword', component:ForgotPasswordComponent},
-  {path: 'newpassword', component:NewPasswordComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
