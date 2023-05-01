f2ac143ffd4f806f164e143c08dfde68e6843ee6
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2023-05-01 13:46:31 -0500

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterInformationComponent } from './components/register-information/register-information.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { NewPasswordComponent } from './components/new-password/new-password.component';
-import { canActivate, redirectUnauthorizedTo }  from '@angular/fire/auth-guard';
-import { HomeMainComponent } from './components/home-main/home-main.component';
-import { MyMessagesComponent } from './components/my-messages/my-messages.component';
-
-const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
-  // {path: '**', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'login', component:LoginComponent},
-  {path: 'mymessages', component:MyMessagesComponent},
-  {path: 'register', component:RegisterComponent},
-  {path: 'register/information', component:RegisterInformationComponent},
-  {path: 'forgotpassword', component:ForgotPasswordComponent},
-  {path: 'newpassword', component:NewPasswordComponent},
-  {path: 'home', component:HomeMainComponent,...canActivate(()=>redirectUnauthorizedTo(['/login']))}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ResumeComponent } from './components/resume/resume.component';
+import { PublishedEventComponent } from './components/published-event/published-event.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { MaterialModule } from 'shared/material.module'
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterInformationComponent } from './components/register-information/register-information.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { NewPasswordComponent } from './components/new-password/new-password.component';
-import { NavbarLoginComponent } from './components/navbar-login/navbar-login.component';
-import { FooterLoginComponent } from './components/footer-login/footer-login.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-
-import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeMainComponent } from './components/home-main/home-main.component';
-import { MyMessagesComponent } from './components/my-messages/my-messages.component';
-
+import{MatIconModule} from'@angular/material/icon';
+import{MatToolbarModule} from'@angular/material/toolbar';
+import{MatListModule} from '@angular/material/list'
+import{MatButtonModule} from'@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import{MatTabsModule} from'@angular/material/tabs';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    RegisterInformationComponent,
-    ForgotPasswordComponent,
-    NewPasswordComponent,
-    NavbarLoginComponent,
-    FooterLoginComponent,
-    FooterComponent,
-    HeaderComponent,
-    HomeMainComponent,
-    MyMessagesComponent
+    ResumeComponent,
+    PublishedEventComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule,
-    RouterModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
