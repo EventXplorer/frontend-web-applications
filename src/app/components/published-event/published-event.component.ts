6ff4aadf37c6b06edf0cd781a7ec7aefe1545689
FaridIzquierdo <u202015215@upc.edu.pe>
2023-05-22 18:20:36 -0500

    Add section of Filter events by category in Published Event

--- src/app/components/published-event/published-event.component.ts
+++ src/app/components/published-event/published-event.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-published-event',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./published-event.component.css']
 })
 export class PublishedEventComponent {
+  displayedColumns: string[] = ['name', 'photo', 'organizator', 'city', 'category', 'date', 'actions'];
+  data: any[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+
+  @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
+
+  events=[];
+
+  constructor() {}
+
+  getNameByCategory(row: any){
+    //Example
+    //this.router.navigateByUrl(`/detail/${row.position}`);
+  }
 
 }
