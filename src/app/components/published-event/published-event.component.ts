fa9138e541cd2f85c76e79cfb17997dbd4858b00
JennyBalceda <89040419+JennyBalceda@users.noreply.github.com>
2023-06-23 22:05:57 -0500

    Add files via upload

--- src/app/components/published-event/published-event.component.ts
+++ src/app/components/published-event/published-event.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { HttpEventService } from 'src/app/services/http-event.service';
 
 
 @Component({
@@ -12,13 +14,48 @@ export class PublishedEventComponent {
   displayedColumns: string[] = ['name', 'photo', 'organizator', 'city', 'category', 'date', 'actions'];
   data: any[] = [];
   dataSource = new MatTableDataSource<any>(this.data);
-
+  events=[];
   @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
 
-  events=[];
+  
 
-  constructor() {}
+  constructor(
+    private eventService: HttpEventService,
+    private router: Router,
+  ) {}
 
+  getEvents(){
+    let eventData;
+    for(let i = 1; i<=10; i++)
+    {
+      this.eventService.getPokemons(i).subscribe
+      (
+        res => {
+          eventData = {
+            name: res.name,
+            date: res.date,
+
+          }
+          this.data.push(eventData)
+          this.dataSource=new MatTableDataSource<any>(this.data);
+          this.dataSource.paginator = this.paginator;
+        },
+        err=>{
+          console.log(err);
+        }
+      );
+    }
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
   getNameByCategory(row: any){
     //Example
     //this.router.navigateByUrl(`/detail/${row.position}`);
---
+++ src/app/services/http-event.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Event } from '../models/event.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpEventService {
+  //Url que retorna toda la lista de events del backend
+  private baseUrl = "https://eventxplorer-backend.azurewebsites.net/event"; 
+
+  constructor(private httpClient: HttpClient) { }
+  createUser(event: Event): Observable<any> {
+    //console.log(user);
+    return this.httpClient.post(`${this.baseUrl}`, event);}
+
+  getPokemons(index:any){
+      return this.httpClient.get<any>(`${this.baseUrl}/${index}`);
+  }
+}
