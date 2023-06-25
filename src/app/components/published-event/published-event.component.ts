35df997ada5d67714134453f4c09bef2b81c939d
Cristhian Orosco <u201921031@upc.edu.pe>
2023-06-25 18:54:54 -0500

    feat(evet): fix problems

--- src/app/components/published-event/published-event.component.ts
+++ src/app/components/published-event/published-event.component.ts
@@ -5,7 +5,8 @@ import { Router } from '@angular/router';
 import { HttpEventService } from 'src/app/services/http-event.service';
 import { UserDataService } from 'src/app/services/user-data.service';
 import { UserService } from 'src/app/services/user.service';
-
+import { User } from 'src/app/models/user.model';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-published-event',
@@ -18,7 +19,21 @@ export class PublishedEventComponent {
   dataSource = new MatTableDataSource<any>(this.data);
   events=[];
 //
-  allEvent:any[]=[];
+  
+  user:User={
+    uid: null,
+    email: null,
+    id: null,
+    name: null,
+    age: null,
+    city: null,
+    country: null,
+    urlPhoto: null,
+    birthday: null,
+    typeIdentification: null,
+    numberIdentification: null, 
+    creditcard: null
+  };
   
   @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
 
@@ -27,7 +42,7 @@ export class PublishedEventComponent {
 
   constructor(
     private eventService: HttpEventService,
-    private router: Router,
+    private router: Router,private UserService: UserService,private userDataService: UserDataService
   ) {}
 
   ngOnInit(): void {
@@ -36,17 +51,51 @@ export class PublishedEventComponent {
     this.getCreatedEvents();
   }
   //
-  getCreatedEvents(){
-    this.eventService.getAllEvents().subscribe(
-      (response:any[])=>{
-        this.allEvent=response;
+  getCreatedEvents() {
+    // Obtener todos los usuarios
+    this.userDataService.getAllUsers().subscribe(
+      (users: User[]) => {
+        // Obtener todos los usuarios
+        const allUsers = users;
+  
+        // Obtener los eventos de todos los usuarios
+        const eventRequests = allUsers.map((user) =>
+          this.eventService.getEventsByUser(user.id)
+        );
+  
+        // Realizar las solicitudes de eventos en paralelo
+        forkJoin(eventRequests).subscribe(
+          (responses: any[]) => {
+              const eventData:any[]=[];
+  
+            // Procesar las respuestas de eventos para cada usuario
+            responses.forEach((events:any[], index) => {
+              const currentUser = allUsers[index];
+              const userEventData = events.map((event:any) => ({
+                urlPhoto: event.urlPhoto,
+                title: event.title,
+                endTime: event.endTime,
+                startTime: event.startTime,
+                address: event.address,
+              }));
+  
+              // Agregar los eventos al arreglo de datos
+                eventData.push(...userEventData);
+            
+            });
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
       },
-      (error)=>{
+      (error) => {
         console.error(error);
       }
     );
   }
-
+  
+//
   getEvents(){
     let eventData;
     for(let i = 1; i<=10; i++)
